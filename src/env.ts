a9938d4b4afded994dae049a0cec57fb3aa4cae2
QuocPham (kai) <anhchang1412@gmail.com>
2021-06-12 09:56:13 +0700

    init

---
+++ src/env.ts
@@ -0,0 +1,5 @@
+export const DOMAIN = 'https://localhost:8080';
+export const API = 'https://localhost:8080';
+
+export const USER_TOKEN = 'USER_TOKEN';
+export const TIME_ZONE = 'Asia/Ho_Chi_Minh';
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => {
+  const content = 'Hello world';
+  return <div>{content}</div>;
+};
+
+export default Home;
